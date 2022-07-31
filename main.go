60b3eb50105f05eb4c92d1e3f5330acffa99d792
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-07-31 20:41:15 +0500

    my event struct

--- main.go
+++ main.go
@@ -1,15 +1,19 @@
 package main
 
+import "github.com/aws/aws-lambda-go/lambda"
+
 type MyEvent struct {
+	Name string `json:"what is ur name?"`
+	Age  int    `json:"how old are u?"`
 }
 
 type MyResponse struct {
 }
 
-func HandleMyEvent(event MyEvent) (MyResponse, error) {
+func HandleLambdaEvent(event MyEvent) (MyResponse, error) {
 	return MyResponse
 }
 
 func main() {
-
+	lambda.Start(HandleLambdaEvent)
 }
