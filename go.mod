b476b779bea2c204cc7012c9d9912ed0be2b0c22
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-07-31 20:38:16 +0500

    init

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go_lambda
 
 go 1.17
+
+require github.com/aws/aws-lambda-go v1.34.1
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+type MyEvent struct {
+}
+
+type MyResponse struct {
+}
+
+func HandleMyEvent(event MyEvent) (MyResponse, error) {
+	return MyResponse
+}
+
+func main() {
+
+}
